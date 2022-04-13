6234aafab5a951b1fd12e7fbe7357291e80c8458
linsolas <linsolas@gmail.com>
2022-04-13 19:25:23 +0200

    update last page

--- src/routes/End.tsx
+++ src/routes/End.tsx
@@ -1,23 +1,24 @@
 const unseen = [
   'Mémoire',
- 'Application',
- 'Android Remote debugging',
- 'Animations',
- 'Synchronisation de la configuration',
- 'etc.'
+  'Application',
+  'Debug',
+  'Android debugging',
+  'Animations',
+  'Network',
+  'Configuration synchronisée',
+  'etc.'
 ];
 
 export const EndPage = () => (
   <div className="p-5">
     <div className="container-fluid">
-      <h5 className="row">Et bien d'autres choses encore...</h5>
       <div className="row g-3">
         {unseen.map((item: string, index: number) => (
           <div className="col-3" key={item}>
-            <div className="p-3 border bg-white">
+            <div className="p-3 border bg-white" style={{fontSize: '1.5rem'}}>
               <span className="text-danger fw-bold">{index + 1}</span>
               <span
-                className="border-start border-2 ps-3 bg-white"
+                className="border-start border-2 ps-2 bg-white"
                 style={{ marginLeft: '-3px'}}
               >
                 {item}
@@ -25,6 +26,48 @@ export const EndPage = () => (
             </div>
           </div>
         ))}
+      </div>
+
+      <div className="row my-5">
+        <div className="col-6">
+          <div className="p-3 border bg-white" style={{fontSize: '1.5rem'}}>
+            <span className="text-danger fw-bold">DOC</span>
+            <a
+              className="border-start border-2 ps-2 bg-white"
+              style={{ marginLeft: '-3px'}}
+              href="https://developer.chrome.com/docs/devtools/"
+              target="_blank"
+            >
+              https://developer.chrome.com/docs/devtools/
+            </a>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="p-3 border bg-white" style={{fontSize: '1.5rem'}}>
+            <span className="text-danger fw-bold">BLOG</span>
+            <a
+              className="border-start border-2 ps-2 bg-white"
+              style={{ marginLeft: '-3px'}}
+              href="https://developer.chrome.com/blog/"
+              target="_blank"
+            >
+              https://developer.chrome.com/blog/
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="homepage d-flex align-items-center justify-content-center p-5"
+        style={{ height: 'auto', marginLeft: '-100px', marginRight: '-100px'}}
+      >
+        <p className="p-2">
+          <span className="title">❤️ MERCI ❤️</span>
+        </p>
+      </div>
+
+      <div className="d-none text-center display-1 border-top p-3 mt-3">
+        ❤️ MERCI ❤️ 
       </div>
     </div>
   </div>
