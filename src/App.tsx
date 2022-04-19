7cdf413f010b9898a9d875094528e8c1d3b272a9
linsolas <linsolas@gmail.com>
2022-04-19 18:56:15 +0200

    Finally fix Github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,6 @@ import { FlexPage } from './routes/Flex';
 import { EndPage } from './routes/End';
 import { ConsolePage } from './routes/Console';
 
-// export const App = () => (
-//   <BrowserRouter>
-//     <Header/>
-//     <Routes>
-//       <Route path="/form" element={<FormPage/>}/>
-//       <Route path="/flex" element={<FlexPage/>}/>
-//       <Route path="/console" element={<ConsolePage/>}/>
-//       <Route path="/end" element={<EndPage/>}/>
-//       <Route path="*" element={<HomePage/>}/>
-//     </Routes>
-//   </BrowserRouter>
-// );
-
 export const App = () => (
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Header/>
@@ -28,7 +15,6 @@ export const App = () => (
       <Route path="/console" element={<ConsolePage/>}/>
       <Route path="/end" element={<EndPage/>}/>
       <Route path="*" element={<HomePage/>}/>
-      <Route path={process.env.PUBLIC_URL} element={<HomePage/>}/>
     </Routes>
   </BrowserRouter>
 );
