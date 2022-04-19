ba0b33c13c0f4b232a4e6ef209a30b1036b05776
linsolas <linsolas@gmail.com>
2022-04-19 18:30:56 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,30 @@ import { FlexPage } from './routes/Flex';
 import { EndPage } from './routes/End';
 import { ConsolePage } from './routes/Console';
 
-export const App = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Header/>
-    <Routes>
-      <Route path="/form" element={<FormPage/>}/>
-      <Route path="/flex" element={<FlexPage/>}/>
-      <Route path="/console" element={<ConsolePage/>}/>
-      <Route path="/end" element={<EndPage/>}/>
-      <Route path="*" element={<HomePage/>}/>
-    </Routes>
-  </BrowserRouter>
-);
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/form" element={<FormPage/>}/>
+        <Route path="/flex" element={<FlexPage/>}/>
+        <Route path="/console" element={<ConsolePage/>}/>
+        <Route path="/end" element={<EndPage/>}/>
+        <Route path="*" element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+// export const App = () => (
+//   <BrowserRouter basename={process.env.PUBLIC_URL}>
+//     <Header/>
+//     <Routes>
+//       <Route path="/form" element={<FormPage/>}/>
+//       <Route path="/flex" element={<FlexPage/>}/>
+//       <Route path="/console" element={<ConsolePage/>}/>
+//       <Route path="/end" element={<EndPage/>}/>
+//       <Route path="*" element={<HomePage/>}/>
+//     </Routes>
+//   </BrowserRouter>
+// );
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
-// import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import React from 'react';
 import './index.scss';
-import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode><App /></React.StrictMode>,
-  document.getElementById('root')
+const root = createRoot(document.querySelector('#root')!);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
-
-// const root = createRoot(document.querySelector('#root')!);
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// );
