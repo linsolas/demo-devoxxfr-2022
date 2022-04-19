d09e61d1c8879dc0b835bed497f841f6d713a9cf
linsolas <linsolas@gmail.com>
2022-04-19 18:21:55 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import React from 'react';
 import './index.scss';
+import ReactDOM from 'react-dom';
 
-const root = createRoot(document.querySelector('#root')!);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(
+  <React.StrictMode><App /></React.StrictMode>,
+  document.getElementById('root')
 );
+
+// const root = createRoot(document.querySelector('#root')!);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
